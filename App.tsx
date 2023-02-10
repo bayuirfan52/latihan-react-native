293af5ad52ffe415839c01d51889b9149d440c6c
bayuirfan52 <bayuirfan52@gmail.com>
2023-02-10 09:57:45 +0700

    state menggunakan class

--- App.tsx
+++ App.tsx
@@ -1,24 +1,43 @@
-import React, { useState } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+class App extends Component {
+  state = {
+    counter: 0,
+  }
 
-  return (
-    <View style={styles.container}>
-      <Text>Anda sudah menekan tombol sebanyak {count} kali</Text>
-      
-      <Button onPress={() => setCount(count + 1)} title='Klik' />
-    </View>
-  );
+  onPress = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text>Klik</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>Anda sudah menekan sebanyak {this.state.counter} kali</Text>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-  }
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10,
+  },
 });
 
 export default App;
