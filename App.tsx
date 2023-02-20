c5a7cb46d6d5c7ac162c1afa206822b4e5a356d2
bayuirfan52 <bayuirfan52@gmail.com>
2023-02-20 18:19:54 +0700

    text input

--- App.tsx
+++ App.tsx
@@ -1,43 +1,33 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { Component, useState } from "react";
+import { View, Text, ScrollView, Image, TextInput } from "react-native";
 
-class App extends Component {
-  state = {
-    counter: 0,
-  }
-
-  onPress = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={this.onPress}>
-          <Text>Klik</Text>
-        </TouchableOpacity>
-        <View>
-          <Text>Anda sudah menekan sebanyak {this.state.counter} kali</Text>
-        </View>
+const App = () => {
+  const [text, setText] = useState('');
+  return (
+    <ScrollView
+      style={{
+        padding: 16
+      }}>
+      <Text>Coba Scroll View</Text>
+      <View>
+        <Text>{text}</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png'
+          }}
+          style={{ width: 200, height: 200 }} />
       </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    marginBottom: 10,
-  },
-});
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          padding: 8
+        }}
+        onChangeText={text => setText(text)}
+        placeholder="Ini adalah hint"/>
+    </ScrollView>
+  );
+};
 
 export default App;
